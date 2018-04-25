5597c3d9892674463d8d428c19e138b7a03818f3
farzanurifan <farzanurifan@gmail.com>
2018-04-25 16:14:42 +0700

    Sample page

--- resources/views/inc/sidebar.blade.php
+++ resources/views/inc/sidebar.blade.php
@@ -6,8 +6,8 @@
             <img src="{{{ asset('images/user.png') }}}" width="48" height="48" alt="User" />
         </div>
         <div class="info-container">
-            <div class="name" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">aa</div>
-            <div class="email">aa#gmail.com</div>
+            <div class="name" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">{{ Auth::user()->name }}</div>
+            <div class="email">{{ Auth::user()->email }}</div>
         </div>
     </div>
     <!-- #User Info -->
@@ -15,6 +15,36 @@
     <div class="menu">
         <ul class="list">
             <li class="header">MAIN NAVIGATION</li>
+            <li class="active">
+                <a href="/student">
+                    <i class="material-icons">home</i>
+                    <span>Student Masterlist</span>
+                </a>
+            </li>
+            <li class="inactive">
+                <a href="/home">
+                    <i class="material-icons">home</i>
+                    <span>All Classes</span>
+                </a>
+            </li>
+            <li class="inactive">
+                <a href="/home">
+                    <i class="material-icons">home</i>
+                    <span>Enroll Student</span>
+                </a>
+            </li>
+            <li class="inactive">
+                <a href="/home">
+                    <i class="material-icons">home</i>
+                    <span>Grades</span>
+                </a>
+            </li>
+            <li class="inactive">
+                <a href="/home">
+                    <i class="material-icons">home</i>
+                    <span>Registrar Setings</span>
+                </a>
+            </li>
         </ul>
     </div>
     <!-- #Menu -->
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,16 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::group(['prefix' => 'student'], function(){
+    Route::get('/', 'StudentController@index');
+    Route::get('/create', 'StudentController@create');
+    Route::post('/create/submit','StudentController@create_submit');
+    Route::get('/edit', 'StudentController@edit');
+    Route::put('/edit/submit','StudentController@edit_submit');
+    Route::delete('/delete', 'StudentController@delete')->name('student.delete');
+});
+
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
