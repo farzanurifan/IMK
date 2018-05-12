f5142ef09c6dcf906dc25a044024c204d07f23ed
IlhamYanto <mohilhamfebriyanto.7a.14@gmail.com>
2018-05-12 14:12:39 +0700

    Hapus Create ID

--- resources/views/pages/staf/staff/create.blade.php
+++ resources/views/pages/staf/staff/create.blade.php
@@ -20,19 +20,19 @@
                 </div>
                 <div class="body col-md-offset-1 col-xs-offset-1 col-sm-offset-1 col-lg-offset-1">
                     {!! Form::open(['action' => 'Staf\StaffController@create_submit', 'id'=>'form_validation', 'method'=>'POST','class'=>'form-horizontal']) !!}
-                        <div class="row clearfix">
+                        <!-- <div class="row clearfix">
                             <div class="col-md-3 col-xs-3 col-sm-3 col-lg-3 form-label" style="text-align: left;margin-top:8px;">
                                 {{Form::label('id','ID')}}
                             </div>
                             <div class="col-md-3 col-xs-3 col-sm-3 col-lg-3">
                                 <div class="form-group">
                                     <div class="m-t-5">
-                                        {{Form::text('id','3',[ 'disabled' ])}}
+                                        {{Form::text('id','1',[ 'disabled' ])}}
                                     </div>
                                     <small>*id otomatis terisi</small>
                                 </div>
                             </div>
-                        </div>
+                        </div> -->
                         <div class="row clearfix">
                             <div class="col-md-3 col-xs-3 col-sm-3 col-lg-3 form-label" style="text-align: left;margin-top:8px;">
                                 {{Form::label('namastaff','Nama Staf')}}
